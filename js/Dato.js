a0da1a4143d0738f4c18921564f402f3b6d5f60a
JuanFernandoUribeRocha <fernandouribe.tech@gmail.com>
2026-08-07 20:11:34 -0700

    Proyecto de calculadora de presupuesto

---
+++ js/Dato.js
@@ -0,0 +1,25 @@
+class Dato {
+  _descripcion;
+  _valor;
+
+  constructor(descripcion, valor) {
+    this._descripcion = descripcion;
+    this._valor = valor;
+  }
+
+  set descripcion(descripcion) {
+    this._descripcion = descripcion;
+  }
+
+  get descripcion() {
+    return this._descripcion;
+  }
+
+  set valor(valor) {
+    this._valor = valor;
+  }
+
+  get valor() {
+    return this._valor;
+  }
+}
